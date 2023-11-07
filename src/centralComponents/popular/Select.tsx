24ad4c9a0aee0eef8ea64df20134b092bbb00879
g-dolidze <124455439+g-dolidze@users.noreply.github.com>
2023-11-07 12:52:47 +0400

    central segments

---
+++ src/centralComponents/popular/Select.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+export default function BasicSelect() {
+  const [timeFilter, setTimeFilter] = React.useState("Daily");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTimeFilter(event.target.value as string);
+  };
+
+  return (
+    <Box>
+      <FormControl fullWidth>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={timeFilter}
+          label="timeFilter"
+          onChange={handleChange}
+          sx={{
+            padding: "0px",
+            fontSize: "12px",
+            fontWeight: "400",
+            color: "#121111",
+            boxShadow: "none",
+            ".MuiOutlinedInput-notchedOutline": { border: 0 },
+            "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+              border: 0,
+            },
+            "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+              {
+                border: 0,
+              },
+          }}
+        >
+          <MenuItem value={"Daily"}>Daily</MenuItem>
+          <MenuItem value={"Monthly"}>Monthly</MenuItem>
+          <MenuItem value={"Annual"}>Annual</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
